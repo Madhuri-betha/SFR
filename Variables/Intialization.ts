caba8d768b28855719a8ffcdd70826ada8ccebf3
Madhuri-betha <madhurib@backflipt.com>
2023-12-26 12:34:05 +0530

    remove variables

--- Variables/Intialization.ts
+++
@@ -1,7 +0,0 @@
-export const loginCredentials = {
-    email: "adityal@backflipt.com",
-    password: "Xenovus@123"
-}
-export const app = {
-    APP_URL: "https://staging-flows.backflipt.com/apps/64a797a05dcdd59488b02f1d#login"
-}
